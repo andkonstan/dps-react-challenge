f90a58fc48d6c7012c00c1aaafeb739338d866d8
andkonstan <4963727+andkonstan@users.noreply.github.com>
2025-05-17 11:22:47 +0200

    Add name and city fields; added filters first logic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import dpsLogo from './assets/DPS.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import UserTable from './components/UserTable';
+import { User } from './types/User';
 function App() {
 	const [users, setUsers] = useState<User[]>([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [selectedCity, setSelectedCity] = useState('');
+	const uniqueCities = Array.from(new Set(users.map(user => user.address.city)));
 	const [loading, setLoading] = useState(true);
+	const filteredUsers = users
+		.filter(user =>
+			(user.firstName + ' ' + user.lastName).toLowerCase().includes(searchTerm.toLowerCase())
+		)
+		.filter(user =>
+			selectedCity === '' || user.address.city === selectedCity
+		);
 	useEffect(() => {
 		fetch('https://dummyjson.com/users')
 			.then(res => res.json())
@@ -28,8 +39,30 @@ function App() {
 				</a>
 			</div>
 			<div className="home-card">
+				<div style={{ display: 'flex', gap: '1rem', marginBottom: '1.5rem' }}>
+					<input
+						type="text"
+						placeholder="Search by name..."
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+						style={{ padding: '0.5rem', flex: 2 }}
+					/>
+
+					<select
+						value={selectedCity}
+						onChange={(e) => setSelectedCity(e.target.value)}
+						style={{ padding: '0.5rem', flex: 1 }}
+					>
+						<option value="">Select city</option>
+						{uniqueCities.map(city => (
+							<option key={city} value={city}>{city}</option>
+						))}
+					</select>
+				</div>
 				<h1>Customer List</h1>
-				<UserTable users={users} />
+				
+				<UserTable users={filteredUsers} />
+				
 			</div>
 		</>
 	);
